1e3000477ff71990549089c305738a4c128f6dfc
F A M <70080501+Kdrew6723145@users.noreply.github.com>
2022-10-12 13:15:09 -0400

    Add files via upload

---
+++ pregunta 2/app/Views/cabecera.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PREGUNTA 1</title>
+    <link rel="stylesheet" href="<?php echo $es ?>">
+</head>
+
+<body>
+
+    <ul>
+        <li><a class="active" href="/pregunta1">INICIO</a></li>
+        <li style="float:right"><a href="#">ACCEDER</a></li>
+    </ul>
---
+++ pregunta 2/app/Views/pie.php
@@ -0,0 +1,82 @@
+</div>
+
+</main>
+</body>
+<footer>
+                <div class="container">
+                    <div class="site-footer-primary-section-1 site-footer-section site-footer-section-1">
+                        <aside class="footer-widget-area widget-area site-footer-focus-item" data-section="sidebar-widgets-footer-widget-2" aria-label="Footer Widget 2">
+                            <div class="footer-widget-area-inner site-info-inner">
+                                <section id="block-14" class="widget widget_block widget_text">
+                                    <p></p>
+                                </section>
+                                <section id="block-24" class="widget widget_block widget_media_image">
+                                    <div class="wp-block-image">
+                                        <figure class="aligncenter size-large is-resized"><img loading="lazy" src="http://www.fcpn.edu.bo/wp-content/uploads/2021/08/UMSA-logo-neg-1024-507x1024.png" alt="" class="wp-image-3184" width="57" height="114" srcset="http://www.fcpn.edu.bo/wp-content/uploads/2021/08/UMSA-logo-neg-1024-507x1024.png 507w, http://www.fcpn.edu.bo/wp-content/uploads/2021/08/UMSA-logo-neg-1024-149x300.png 149w, http://www.fcpn.edu.bo/wp-content/uploads/2021/08/UMSA-logo-neg-1024-768x1551.png 768w, http://www.fcpn.edu.bo/wp-content/uploads/2021/08/UMSA-logo-neg-1024-761x1536.png 761w, http://www.fcpn.edu.bo/wp-content/uploads/2021/08/UMSA-logo-neg-1024-1014x2048.png 1014w, http://www.fcpn.edu.bo/wp-content/uploads/2021/08/UMSA-logo-neg-1024.png 1024w" sizes="(max-width: 57px) 100vw, 57px"></figure>
+                                    </div>
+                                </section>
+                            </div>
+                        </aside>
+                    </div>
+                    <div class="site-footer-primary-section-2 site-footer-section site-footer-section-2">
+                        <aside class="footer-widget-area widget-area site-footer-focus-item" data-section="sidebar-widgets-footer-widget-1" aria-label="Footer Widget 1">
+                            <div class="footer-widget-area-inner site-info-inner">
+                                <section id="block-12" class="widget widget_block widget_text">
+                                    <p></p>
+                                </section>
+                                <section id="block-22" class="widget widget_block widget_media_image">
+                                    <div class="wp-block-image">
+                                        <figure class="aligncenter size-full is-resized"><img loading="lazy" src="http://www.fcpn.edu.bo/wp-content/uploads/2021/08/FCPN-CLANCO-png.png" alt="" class="wp-image-2420" width="84" height="84" srcset="http://www.fcpn.edu.bo/wp-content/uploads/2021/08/FCPN-CLANCO-png.png 1024w, http://www.fcpn.edu.bo/wp-content/uploads/2021/08/FCPN-CLANCO-png-300x300.png 300w, http://www.fcpn.edu.bo/wp-content/uploads/2021/08/FCPN-CLANCO-png-150x150.png 150w, http://www.fcpn.edu.bo/wp-content/uploads/2021/08/FCPN-CLANCO-png-768x768.png 768w" sizes="(max-width: 84px) 100vw, 84px"></figure>
+                                    </div>
+                                </section>
+                            </div>
+                        </aside>
+                    </div>
+
+                    <div class="site-footer-primary-section-3 site-footer-section site-footer-section-3">
+                        <div class="footer-widget-area widget-area site-footer-focus-item ast-footer-html-1" data-section="section-fb-html-1">
+                            <div class="ast-header-html inner-link-style-">
+                                <div class="ast-builder-html-element">
+                                    <p>
+                                        <!-- wp:paragraph {"fontSize":"medium"} -->
+                                    </p>
+                                    <p><strong>INFORMACIÓN DE CONTACTO</strong></p>
+                                    <p>
+                                        <!-- wp:paragraph -->Dirección: Av. Villazón Nº 1995, Plaza del Bicentenario - Zona Central, La Paz - Bolivia.<br>
+                                        Telf.: (591-2) 2441570 / 2440571<br>
+                                        Fax: 2441092<br>
+                                        Email: decanato@fcpn.edu.bo
+                                    </p>
+                                    <p style="text-align: left;">
+                                        <!-- /wp:paragraph -->
+                                        <!-- wp:paragraph -->
+                                    </p>
+                                    <p>
+                                        <!-- /wp:paragraph -->
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="site-footer-primary-section-4 site-footer-section site-footer-section-4">
+                        <div class="ast-builder-layout-element ast-flex site-footer-focus-item" data-section="section-fb-social-icons-1">
+                            <div class="ast-footer-social-1-wrap ast-footer-social-wrap">
+                                <div class="footer-social-inner-wrap element-social-inner-wrap social-show-label-true ast-social-color-type-custom ast-social-stack-none ast-social-element-style-filled"><a href="https://www.facebook.com/fcpn.umsa" aria-label="Síguenos" en="" facebook="" target="_blank" rel="noopener noreferrer" style="--color: #557dbc; --background-color: transparent;" class="ast-builder-social-element ast-inline-flex ast-facebook footer-social-item"><span class="ahfb-svg-iconset ast-inline-flex svg-baseline"><svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" viewBox="0 0 448 512">
+                                                <path d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z"></path>
+                                            </svg></span><span class="social-item-label">Síguenos en Facebook</span></a><a href="https://www.youtube.com/channel/UCziXp6vAr6dQMfgz_dy_hNA" aria-label="Síguenos" en="" youtube="" target="_blank" rel="noopener noreferrer" style="--color: #e96651; --background-color: transparent;" class="ast-builder-social-element ast-inline-flex ast-youtube footer-social-item"><span class="ahfb-svg-iconset ast-inline-flex svg-baseline"><svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" viewBox="0 0 576 512">
+                                                <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"></path>
+                                            </svg></span><span class="social-item-label">Síguenos en Youtube</span></a></div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+                </div>
+
+</footer>
+
+</html>
