8b2a4fcbba6fc62a6e0e60fb9a2a35ae1606f5e6
F A M <70080501+Kdrew6723145@users.noreply.github.com>
2022-10-12 13:10:00 -0400

    Add files via upload

---
+++ pregunta1/index.php
@@ -0,0 +1,51 @@
+<?php 
+$es="estilo.css";
+include "cabecera.php"
+?>
+
+    <main class="contenedor sombra">
+        <div class="contenido-inicio">
+            <div class="imagen-scomrest">
+                <img src="images/fcppp.png" alt="">
+            </div>
+            <div class="parrafos">
+                    <p>INFORMATICA</p>
+                    <p>FISICA</p>
+                    <p>QUIMICA</p>
+                    <p>BIOLOGIA</p>
+                    <p>ESTADISTICA</p>
+                    <p>MATEMATICAS</p>
+                </div>
+            <div class="carreras">
+                
+                <a href="informatica/">
+                    <img src="images/informatica.jpg" alt="">
+                </a>
+                <a href="fisica/">
+                    <img src="images/fis.jpg" alt="">
+                </a>
+                <a href="quimica/">
+                    <img src="images/quimica.png" alt="">
+                </a>
+
+                <a href="biologia/">
+                    <img src="images/biologia.png" alt="">
+                </a>
+                <a href="estadistica/">
+                    <img src="images/estadis.jpg" alt="">
+                </a>
+                <a href="matematicas/">
+                    <img src="images/mate.png" alt="">
+                </a>
+
+
+            </div>
+
+       
+
+    <?php include 'pie.php'?>
+
+
+
+
+
---
+++ pregunta1/matematicas/index.php
@@ -0,0 +1,40 @@
+<?php 
+$es="../estilo.css";
+include "../cabecera.php"
+?>
+
+<style type="text/css">
+
+    .volver{
+        margin: 10 10;
+        padding-top: 50px;
+        padding-bottom: 50px;
+    }
+
+    .volver {
+       text-size-adjust: 50px;
+       margin-right: 50px;
+    }
+
+
+
+</style>
+
+
+<div class="texto">
+    <h1 align="center">MATEMATICAS</h1>
+    <div class="volver">
+        <a href="../" align="right">
+
+            <span ><h1>volver</h1></span>
+
+        </a>
+    </div>
+
+
+</div>
+
+
+<?php 
+include "../pie.php"
+?>
